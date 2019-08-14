8e73c8e6aa7ecc65eb64e4676b1d480feaf2a2bf
Dragonite <haolin97@gmail.com>
2019-08-14 16:22:49 +0800

    Create models for case_study, medication, question, tag and mhx

--- core/case_study/models.py
+++ core/case_study/models.py
@@ -1,5 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 
-# added random comment
+# blank=True means that the field is not required
+class Question(models.Model):
+    body = models.TextField(help_text='Question text body')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=60)
+
+
+class CaseStudy(models.Model):
+    # Years Choices
+    YEARS = 'Y'
+    MONTHS = 'M'
+    AGE_CHOICES = [
+        (YEARS, 'Years'),
+        (MONTHS, 'Months')
+    ]
+    # Sex Choices
+    MALE = 'M'
+    FEMALE = 'F'
+    SEX_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female')
+    ]
+    # Processing information and settings
+    date_submitted = models.DateTimeField(null=True, blank=True)
+    date_last_edited = models.DateTimeField(null=True, blank=True)
+    last_edited_user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    is_deleted = models.BooleanField(default=False)
+    # Case study fields
+    height = models.IntegerField(blank=True)
+    weight = models.IntegerField(blank=True)
+    scr = models.IntegerField(blank=True)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    age_type = models.CharField(
+        max_length=1,
+        choices=AGE_CHOICES,
+        default=YEARS
+    )
+    age = models.IntegerField()
+    sex = models.CharField(
+        max_length=1,
+        choices=SEX_CHOICES,
+        default=MALE
+    )
+    description = models.TextField(help_text='Description for case study scenario.')
+    # Case Study Question and Answer
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_1 = models.TextField(help_text='Answer 1')
+    answer_2 = models.TextField(help_text='Answer 2')
+    answer_3 = models.TextField(help_text='Answer 3')
+    answer_4 = models.TextField(help_text='Answer 4')
+    feedback = models.TextField(help_text='Case study creator feedback')
+
+class MedicalHistory(models.Model):
+    body = models.TextField(help_text='Medical history information')
+    case_study = models.ForeignKey(CaseStudy, on_delete=models.CASCADE)
+
+class Medication(models.Model):
+    name = models.TextField(help_text='Medication Name')
+    case_study = models.ForeignKey(CaseStudy, on_delete=models.CASCADE)
+
+
+
--- core/core/settings.py
+++ core/core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'case_study.apps.CaseStudyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
