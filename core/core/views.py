fca0960577e82cae00fad15326993d2cc3934e0b
Dragonite <haolin97@gmail.com>
2019-08-14 15:04:10 +0800

    Link case study module to overall django app

--- core/core/views.py
+++ core/core/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the case study index.")
+    return HttpResponse("Please use the /admin or /cases routes.")
