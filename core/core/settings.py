b6f496d78d26def6a7cbd68255c3b54110565ec0
Tim <tim@tim-ings.com>
2019-08-15 16:20:07 +0800

    Adds homepage template

--- core/core/settings.py
+++ core/core/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'static/templates/'),
+            os.path.join(BASE_DIR, 'venv/Lib/site-packages/django/contrib/admin/static/admin'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
