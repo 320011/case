5d2a84827d0c99a7649ce2bc9986efd5d3b52f15
Dragonite <haolin97@gmail.com>
2019-08-15 09:48:11 +0800

    Create view and template temporarily

--- core/case_study/views.py
+++ core/case_study/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
+from case_study.models import (
+    Tag,
+    CaseStudy
+)
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the case study index.")
 
+
 def create_new_case(request):
-    return HttpResponse("Create new case")
+    cases = CaseStudy.objects.all()
+    print(cases)
+    c = {
+        'cases': cases
+    }
+    return render(request, 'create_new_case.html', c)
+    # return HttpResponse("Hello, world. yo!")
