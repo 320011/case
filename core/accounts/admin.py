56807ff2863d5b11bf6ef7ec062c38c014742c10
JD <gajera.jaydeep98@gmail.com>
2019-08-16 21:25:21 +0800

    Register custom user model with Django admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    '''Define admin model for custom User model with email field.'''
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('date_joined')}),)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),)
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
