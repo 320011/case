c822d7be0c8663382d976c6eeede8149f1daf51c
Tim <tim@tim-ings.com>
2019-08-16 14:42:23 +0800

    Adds case cards to user profile

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 
 def profile(request, user_id):
-    c = {}
+    c = {
+        "user_cases": [
+            {
+                "title": "Case 1: XYZ",
+                "description": "This is a cool case description provided by a user. It is pretty long. It just doesnt stop does it.",
+                "pass_rate": 0.75,
+                "view_count": 565688,
+                "patient_sex": "M",
+                "patient_age": 86
+            },
+        ] * 5
+    }
     return render(request, "profile-cases.html", c)
 
 
