73da5868f99877ff35d4427b3dd90a3c5b2af9b4
Hugo Zhang <Hugozhang0521@gmail.com>
2019-08-18 14:25:48 +0800

    set up basic registration form

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.urls import reverse
 
 def home(request):
     return render(request,'accounts/home.html')
 
 def register(request):
-    return render(request, 'accounts/register.html')
-    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('accounts-home')
+    else:
+        form = UserCreationForm() 
+    return render(request, 'accounts/register.html', {"form": form})
+
 def profile(request, user_id):
     c = {
         "user_cases": [
