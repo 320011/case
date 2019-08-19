ba2b716f38e2f4e79c04034fa5af9acc0cd1ff4e
Aaron Jenkins <aaronjenkins1111@gmail.com>
2019-08-19 16:12:39 +0800

    Adds needed urls

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -12,4 +12,21 @@ urlpatterns = [
     path("profile/results", views.view_profile_results, name="profile_results"),
     path("signup/", views.view_signup, name="signup"),
     path("activate/", views.view_activate, name="activate"),
+    
+   
+    path('password_change', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
+    path('password_change_done', auth_views.PasswordChangeDoneView.as_view(
+        template_name='password_change_done.html'), name='password_change_done'),
+
+    path('password_reset',auth_views.PasswordResetView.as_view(template_name='reset.html'),name='password_reset'),
+
+    path('password_reset_done', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'), name='password_reset_done'),
+    
+    path('password_reset_confirm', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_confirm.html'), name='password_reset_complete'),
+    
+    path('password_reset_complete', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_complete.html'), name='password_reset_complete'),
+
 ]
