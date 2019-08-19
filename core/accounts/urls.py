4ed3b4f0c8bf33acaaf9270ef53340cc7a1932d3
Aaron Jenkins <aaronjenkins1111@gmail.com>
2019-08-19 16:36:28 +0800

    Started password reset form (not yet working)

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -18,15 +18,16 @@ urlpatterns = [
     path('password_change_done', auth_views.PasswordChangeDoneView.as_view(
         template_name='password_change_done.html'), name='password_change_done'),
 
-    path('password_reset',auth_views.PasswordResetView.as_view(template_name='reset.html'),name='password_reset'),
+    path('password_reset', auth_views.PasswordResetView.as_view(
+        template_name='password_reset.html'), name='password_reset'),
 
     path('password_reset_done', auth_views.PasswordResetDoneView.as_view(
         template_name='password_reset_done.html'), name='password_reset_done'),
     
-    path('password_reset_confirm', auth_views.PasswordResetDoneView.as_view(
+    path('password_reset_confirm', auth_views.PasswordResetConfirmView.as_view(
         template_name='password_reset_confirm.html'), name='password_reset_complete'),
     
-    path('password_reset_complete', auth_views.PasswordResetDoneView.as_view(
+    path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(
         template_name='password_reset_complete.html'), name='password_reset_complete'),
 
 ]
