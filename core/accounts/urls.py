fef19d0e268769954f5daec265e9e09c18c13ca2
Aaron Jenkins <aaronjenkins1111@gmail.com>
2019-08-19 16:12:39 +0800

    Adds needed urls

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -15,5 +15,20 @@ urlpatterns = [
     path('profile/',  login_required(views.UserView.as_view()), name='profile'),
     path('signup/', views.signup, name='signup'),
     path('activate/<slug:uidb64>/<slug:token>/', views.activate, name='activate'),
-    path('password_reset',auth_views.PasswordResetView.as_view(template_name='reset.html'),name='password_reset')
+   
+    path('password_change', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
+    path('password_change_done', auth_views.PasswordChangeDoneView.as_view(
+        template_name='password_change_done.html'), name='password_change_done'),
+
+    path('password_reset',auth_views.PasswordResetView.as_view(template_name='reset.html'),name='password_reset'),
+
+    path('password_reset_done', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'), name='password_reset_done'),
+    
+    path('password_reset_confirm', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_confirm.html'), name='password_reset_complete'),
+    
+    path('password_reset_complete', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_complete.html'), name='password_reset_complete'),
+
 ]
