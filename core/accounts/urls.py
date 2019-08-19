cdd3c523240342fb0db349a75d2fc9cd037a1df9
Aaron Jenkins <aaronjenkins1111@gmail.com>
2019-08-19 15:50:35 +0800

    Adds password reset to login page

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('profile/',  login_required(views.UserView.as_view()), name='profile'),
     path('signup/', views.signup, name='signup'),
     path('activate/<slug:uidb64>/<slug:token>/', views.activate, name='activate'),
+    path('password_reset',auth_views.PasswordResetView.as_view(template_name='reset.html'),name='password_reset')
 ]
