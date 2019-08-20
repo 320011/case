b77e4c233090e4ca9dfbe29bca9a54a12e1d95bc
Tim <tim@tim-ings.com>
2019-08-20 11:32:40 +0800

    Moves global templates to account app

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render
 from django.contrib.auth import login
 from django.contrib.sites.shortcuts import get_current_site
@@ -14,7 +15,8 @@ from .tokens import account_activation_token
 @login_required
 def view_profile(request):
     c = {
-        "user_cases": [
+        "title": "Cases | My Profile",
+         "user_cases": [
             {
                 "title": "Case 1: XYZ",
                 "description": "This is a cool case description provided by a user. "
@@ -29,14 +31,16 @@ def view_profile(request):
                 "patient_sex": "M",
                 "patient_age": 86
             },
-        ] * 5
+        ] * 5,
     }
     return render(request, 'profile-cases.html', c)
 
 
 @login_required
 def view_profile_results(request):
-    c = {}
+    c = {
+        "title": "Results | My Profile",
+    }
     return render(request, "profile-results.html", c)
 
 
@@ -47,12 +51,12 @@ def view_signup(request):
             user = form.save(commit=False)
             user.is_active = False
             user.save()
-            current_site = get_current_site(request)
-            message = render_to_string('activate_account.html', {
+            message = render_to_string('mail/email-activate-account.html', {
                 'user': user,
-                'domain': current_site.domain,
+                'domain': get_current_site(request).domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': account_activation_token.make_token(user),
+                "protocol": request.is_secure() and "https" or "http"
             })
             email_subject = 'Activate Your Case Account'
             to_email = form.cleaned_data.get('email')
@@ -60,26 +64,39 @@ def view_signup(request):
             email.send()
             message = {'message': 
                 'The activation link is sent to your email. Please confirm your email address to complete the registration.'}
-            return render(request, 'activate_message.html', message)
+            return render(request, 'activate-message.html', message)
     else:
         form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+    c = {
+        "title": "Sign Up | Case",
+        "form": form
+    }
+    return render(request, 'auth-signup.html', c)
 
 
-def view_activate(request, uidb64, token):
+def view_activate(request):
+    id = request.GET.get("id")
+    token = request.GET.get("token")
+
+    if id is None or token is None:
+        return HttpResponseBadRequest()
+
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_text(urlsafe_base64_decode(id))
         user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
+
+    c = {
+        "message": "Activation link is invalid!"
+    }
+
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
         login(request, user)
-        message = {'message': 'Your account has been successfully activated.'}
-        return render(request, 'activate_message.html', message)
-    else:
-        message = {'message': 'Activation link is invalid!'}
-        return render(request, 'activate_message.html', message)
+        c["message"] = "Your account has been successfully activated."
+
+    return render(request, 'activate-message.html', c)
 
 
