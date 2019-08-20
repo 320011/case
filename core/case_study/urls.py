2d4be541d9b82ed54f1e366b327eab5a045624c6
Aaron Jenkins <aaronjenkins1111@gmail.com>
2019-08-20 19:30:44 +0800

    Tries to resolve merge issues

--- core/case_study/urls.py
+++ core/case_study/urls.py
@@ -5,11 +5,6 @@ from . import views
 app_name = "cases"
 
 urlpatterns = [
-<<<<<<< HEAD
     path("start-new-case/", views.start_new_case, name="start-new-case"),
     path("create-new-case/<int:case_study_id>", views.create_new_case, name="create-new-case"),
 ]
-=======
-    path('', views.index, name='cases'),
-]
->>>>>>> Password reset works using ugly built-in templates. Requires cleanup.
--- core/core/urls.py
+++ core/core/urls.py
@@ -29,7 +29,8 @@ urlpatterns = [
         template_name='password_change_done.html'), name='password_change_done'),
 
     path('password_reset', auth_views.PasswordResetView.as_view(
-        template_name='password_reset.html', email_template_name='password_reset_email.html', subject_template_name='password_reset_subject.txt'), name='password_reset'),
+        template_name='password_reset.html', email_template_name='password_reset_email.html', 
+        subject_template_name='password_reset_subject.txt'), name='password_reset'),
 
     path('password_reset/done', auth_views.PasswordResetDoneView.as_view(
         template_name='password_reset_done.html'), name='password_reset_done'),
