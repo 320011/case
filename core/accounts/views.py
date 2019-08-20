f48ea0cf92ea65e93838742ffda601cffec0f13f
Tim <tim@tim-ings.com>
2019-08-20 11:08:35 +0800

    Integrates accounts templates/views

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.views.generic.detail import DetailView
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import login
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
