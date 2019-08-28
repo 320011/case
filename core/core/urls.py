e732f3ad3b7bbf2e3cd8f52ae2c7b022f1e9dea1
Aaron Jenkins <aaronjenkins1111@gmail.com>
2019-08-28 12:15:53 +0800

    Removed underscores from paths

--- core/core/urls.py
+++ core/core/urls.py
@@ -24,15 +24,15 @@ urlpatterns = [
     path('user/', include('accounts.urls', namespace='accounts')),
     path('admin/', admin.site.urls),
     
-    path('password_change', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
-    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(
+    path('password/change', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
+    path('password/change/done', auth_views.PasswordChangeDoneView.as_view(
         template_name='password_change_done.html'), name='password_change_done'),
 
-    path('password_reset', auth_views.PasswordResetView.as_view(
+    path('password/reset', auth_views.PasswordResetView.as_view(
         template_name='password_reset.html', email_template_name='password_reset_email.html', 
         subject_template_name='password_reset_subject.txt'), name='password_reset'),
 
-    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(
+    path('password/reset/done', auth_views.PasswordResetDoneView.as_view(
         template_name='password_reset_done.html'), name='password_reset_done'),
 
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
