c12336f1daaa7d607d738cff428666e0f2dcc8d6
Hugo Zhang <Hugozhang0521@gmail.com>
2019-09-02 20:53:40 +0800

    add change password functionality and add the names of placeholder for login and change password page

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path("profile/results", views.view_profile_results, name="profile_results"),
     path("signup/", views.view_signup, name="signup"),
     path("activate/", views.view_activate, name="activate"),
-    path("profile/settings", views.view_settings, name="profile_settings")
+    path("profile/settings", views.view_settings, name="profile_settings"),
+    path("profile/changepassword", views.change_password, name="change_password")
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,17 +1,23 @@
 from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login,update_session_auth_hash
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 from django.contrib.auth.decorators import login_required
+<<<<<<< HEAD
 from .forms import UserSettingsForm, SignUpForm
+=======
+from .forms import SignUpForm, UserSettingsForm
+>>>>>>> add change password functionality and add the names of placeholder for login and change password page
 from .models import User
 from .tokens import account_activation_token
 from .decorators import anon_required
 from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
+
 
 @login_required
 def view_profile(request):
@@ -118,3 +124,21 @@ def view_settings(request):
     else:
         form = UserSettingsForm(instance=request.user)
     return render(request, "profile-settings.html", {'form': form})
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'profile-password.html', {
+        'form': form
+    })
