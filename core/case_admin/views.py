c585c05a0cd6a21ba6bcc8ba0ac4b5ffb9047068
Tim <timothyjings@gmail.com>
2019-09-03 22:25:47 +0800

    Adds excel import

--- core/case_admin/views.py
+++ core/case_admin/views.py
@@ -11,6 +11,7 @@ import copy
 import json
 import base64
 from .forms import TagImportForm
+import openpyxl
 
 
 schema_user = {
@@ -310,7 +311,8 @@ def tag_import_txt(request, file, file_format):
     if file.content_type != "text/plain":
         return JsonResponse({
             "success": False,
-            "message": "Failed to import tags as text/plain\n\nPlease ensure your text file contains one tag per line",
+            "message": "Failed to import tags as text/plain\n\n"
+                       "Please ensure your text file contains one tag per line",
         })
     tags = []
     for tag in file.file.readlines():
@@ -325,11 +327,9 @@ def tag_import_txt(request, file, file_format):
     except IntegrityError as e:
         return JsonResponse({
             "success": False,
-            "message": """Failed to import tags as text/plain
-            
-            Please ensure your text file contains one tag per line
-            
-            Error: """ + str(e.args[0]),
+            "message": "Failed to import tags as text/plain\n\n"
+                       "Please ensure your text file contains one tag per line\n\n"
+                       "Error: " + str(e.args[0]),
         })
 
 
@@ -337,7 +337,8 @@ def tag_import_csv(request, file, file_format):
     if file.content_type != "text/csv":
         return JsonResponse({
             "success": False,
-            "message": "Failed to import tags as text/csv\n\nPlease ensure your csv file contains one tag per line",
+            "message": "Failed to import tags as text/csv\n\n"
+                       "Please ensure your csv file contains one tag per line",
         })
     tags = []
     lines = file.read().decode("utf-8").split("\n")
@@ -361,6 +362,64 @@ def tag_import_csv(request, file, file_format):
         })
 
 
+def tag_import_json(request, file, file_format):
+    if file.content_type != "application/json":
+        return JsonResponse({
+            "success": False,
+            "message": "Failed to import tags as application/json\n\n"
+                       "Please ensure your json file contains a list of strings",
+        })
+    tags = []
+    file_text = file.read().decode("utf-8")
+    file_json = json.loads(file_text)
+    for tag in file_json:
+        t = tag.strip()
+        tags.append(Tag(name=t))
+    try:
+        Tag.objects.bulk_create(tags, ignore_conflicts=True)
+        return JsonResponse({
+            "success": True,
+            "message": "Imported {} tags".format(len(tags)),
+        })
+    except IntegrityError as e:
+        return JsonResponse({
+            "success": False,
+            "message": "Failed to import tags as application/json\n\n"
+                       "Please ensure your json file contains a list of strings\n\n"
+                       "Error: " + str(e.args[0]),
+        })
+
+
+def tag_import_xlsx(request, file, file_format):
+    if not (str(file.content_type) == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" or file.name.endswith('.xlsx')):
+        return JsonResponse({
+            "success": False,
+            "message": "Failed to import tags as xlsx\n\n"
+                       "Please ensure column A has a single tag per cell\n\n",
+        })
+    tags = []
+    wb = openpyxl.load_workbook(file)
+    sheet = wb.worksheets[0]
+    for col in sheet.iter_cols():
+        for cel in col:
+            t = str(cel.value).strip()
+            tags.append(Tag(name=t))
+
+    try:
+        Tag.objects.bulk_create(tags, ignore_conflicts=True)
+        return JsonResponse({
+            "success": True,
+            "message": "Imported {} tags".format(len(tags)),
+        })
+    except IntegrityError as e:
+        return JsonResponse({
+            "success": False,
+            "message": "Failed to import tags as xlsx\n\n"
+                       "Please ensure column A has a single tag per cell\n\n"
+                       "Error: " + str(e.args[0]),
+        })
+
+
 @staff_required
 def api_admin_tag_import(request):
     if request.method == "POST":
@@ -372,11 +431,9 @@ def api_admin_tag_import(request):
                 file_format = "csv"
             elif file.content_type == "application/json":
                 file_format = "json"
-            elif file.content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" or \
-                    file.name.endswith('.xlsx'):
+            elif file.content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" or file.name.endswith('.xlsx'):
                 file_format = "xlsx"
-            elif file.content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" or \
-                    file.name.endswith('.xls'):
+            elif file.content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" or file.name.endswith('.xls'):
                 file_format = "xls"
             elif file.content_type == "text/plain":
                 file_format = "txt"
@@ -384,20 +441,11 @@ def api_admin_tag_import(request):
         if file_format == "csv":
             return tag_import_csv(request, file, file_format)
         elif file_format == "json":
-            return JsonResponse({
-                "success": False,
-                "message": "File typ .json is NYI",
-            })
+            return tag_import_json(request, file, file_format)
         elif file_format == "xlsx":
-            return JsonResponse({
-                "success": False,
-                "message": "File typ .xlsx is NYI",
-            })
+            return tag_import_xlsx(request, file, file_format)
         elif file_format == "xls":
-            return JsonResponse({
-                "success": False,
-                "message": "File typ .xls is NYI",
-            })
+            return tag_import_xlsx(request, file, file_format)
         elif file_format == "txt":
             return tag_import_txt(request, file, file_format)
         else:
