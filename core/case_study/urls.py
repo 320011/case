4a1e13aaa3454231833595180eaa3a5ba1e60f31
Dragonite <haolin97@gmail.com>
2019-09-16 12:47:42 +0800

    Remove test view and add moment.js

--- core/case_study/urls.py
+++ core/case_study/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path("ajax/validate_answer/<int:case_study_id>/", views.validate_answer, name="validate-answer"),
     path("ajax/view_comments/<int:case_study_id>/", views.show_comments, name="show-comments"),
     path("ajax/submit_comment/<int:case_study_id>/", views.submit_comment, name="submit-comment"),
-    path("test_view/<int:case_study_id>/", views.test_view, name="test-view")
 ]
