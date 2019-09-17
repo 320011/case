88e3b6d137647f524d26f9d5d8d6c72d5a24270b
Tim <timothyjings@gmail.com>
2019-09-18 00:01:38 +0800

    Entire site has more consistent css

--- core/accounts/templatetags/css.py
+++ core/accounts/templatetags/css.py
@@ -2,9 +2,19 @@ from django import template
 register = template.Library()
 
 
+@register.filter(name='addclass')
+def addclass(field, c=None):
+    return field.as_widget(attrs={"class": c})
+
+
 @register.filter(name='addbootstrapstyle')
 def addbootstrapstyle(field, placeholder=None):
     if placeholder is not None:
         return field.as_widget(attrs={"class": "form-control textarea-sm", "placeholder": placeholder})
     else:
         return field.as_widget(attrs={"class": "form-control textarea-sm"})
+
+
+@register.filter(name='addph')
+def addph(field, ph=None):
+    return field.as_widget(attrs={"placeholder": ph})
