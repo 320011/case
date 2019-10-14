2c1733fb9e01fc214cee471ed373cc0b691d2c0a
Tim <tim@tim-ings.com>
2019-10-14 15:54:18 +0800

    Adds csv export for all models, no filtering

--- core/analytics/urls.py
+++ core/analytics/urls.py
@@ -6,4 +6,16 @@ app_name = "analytics"
 
 urlpatterns = [
     path("", views.view_landing, name='default'),
+    path("question", views.view_question, name='question'),
+    path("tag", views.view_tag, name='tag'),
+    path("medicalhistory", views.view_medicalhistory, name='medicalhistory'),
+    path("comment", views.view_comment, name='comment'),
+    path("casestudy", views.view_casestudy, name='casestudy'),
+    path("medication", views.view_medication, name='medication'),
+    path("commentvote", views.view_commentvote, name='commentvote'),
+    path("tagrelationship", views.view_tagrelationship, name='tagrelationship'),
+    path("other", views.view_other, name='other'),
+    path("commentreport", views.view_commentreport, name='commentreport'),
+    path("attempt", views.view_attempt, name='attempt'),
+    path("user", views.view_user, name='user'),
 ]
--- core/analytics/views.py
+++ core/analytics/views.py
@@ -1,17 +1,88 @@
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.core import serializers
-from django.http import HttpResponseRedirect, JsonResponse
-from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
-from django.utils import timezone
+from django.shortcuts import render
+import csv
+from django.http import HttpResponse
 
-from case_study.models import Question, Tag, CaseStudy, TagRelationship, MedicalHistory, Medication, Other, Attempt, \
+from case_study.models import Question, \
+    Tag, CaseStudy, TagRelationship, \
+    MedicalHistory, Medication, Other, Attempt, \
     Comment, CommentVote, CommentReport
 from accounts.models import User
 
 
-@login_required
+def export_queryset_csv(qs, filename):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+    csvw = csv.writer(response)
+    header_done = False
+    for e in qs:
+        if not header_done:
+            field_names = []
+            for f in e._meta.fields:
+                if f.name != "password":
+                    field_names.append(f.name)
+            csvw.writerow(field_names)
+            header_done = True
+        row_data = []
+        for f in e._meta.fields:
+            if f.name != "password":
+                d = vars(e).get(f.attname, "")
+                if d is None:
+                    d = ""
+                d = str(d).rstrip()
+                row_data.append(d)
+        csvw.writerow(row_data)
+    return response
+
+
 def view_landing(request):
     c = {}
     return render(request, "analytics-landing.html", c)
+
+
+def view_question(request):
+    return export_queryset_csv(Question.objects.all(), "uwacase_questions.csv")
+
+
+def view_tag(request):
+    return export_queryset_csv(Tag.objects.all(), "uwacase_tags.csv")
+
+
+def view_medicalhistory(request):
+    return export_queryset_csv(MedicalHistory.objects.all(), "uwacase_medicalhistories.csv")
+
+
+def view_comment(request):
+    return export_queryset_csv(Comment.objects.all(), "uwacase_comments.csv")
+
+
+def view_casestudy(request):
+    return export_queryset_csv(CaseStudy.objects.all(), "uwacase_casestudies.csv")
+
+
+def view_medication(request):
+    return export_queryset_csv(Medication.objects.all(), "uwacase_medications.csv")
+
+
+def view_commentvote(request):
+    return export_queryset_csv(CommentVote.objects.all(), "uwacase_commentvotes.csv")
+
+
+def view_tagrelationship(request):
+    return export_queryset_csv(TagRelationship.objects.all(), "uwacase_tagrelationships.csv")
+
+
+def view_other(request):
+    return export_queryset_csv(Other.objects.all(), "uwacase_others.csv")
+
+
+def view_commentreport(request):
+    return export_queryset_csv(CommentReport.objects.all(), "uwacase_commentreports.csv")
+
+
+def view_attempt(request):
+    return export_queryset_csv(Attempt.objects.all(), "uwacase_attempts.csv")
+
+
+def view_user(request):
+    return export_queryset_csv(User.objects.all(), "uwacase_users.csv")
+
