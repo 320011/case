85ad136b4046fde89ce6328c507b8242a276830b
Dragonite <haolin97@gmail.com>
2019-10-15 22:14:30 +0800

    Add distinct clause to tag performance dropdown

--- core/analytics/forms.py
+++ core/analytics/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from case_study.models import *
 
 class TagForm(forms.Form):
-    field = forms.ModelChoiceField(queryset=Tag.objects.filter(tagrelationship__isnull=False).order_by('name'))
+    field = forms.ModelChoiceField(queryset=Tag.objects.filter(tagrelationship__isnull=False).distinct().order_by('name'))
 
     def __init__(self, *args, **kwargs):
         super(TagForm, self).__init__(*args, **kwargs)
