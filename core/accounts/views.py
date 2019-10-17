06c21e2b0ec40137cf8c37f1b8a3ce8fc639ca71
JD <gajera.jaydeep98@gmail.com>
2019-10-17 13:22:16 +0800

    adds '%' to the avgs and rounds whole nums in user profile

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -69,6 +69,9 @@ def view_profile(request):
         overall_score = 'N/A'
     else:
         overall_score = float("{0:.2f}".format(total_score/total_tries))
+    # better formats the overall_score
+    if overall_score % 1 == 0:
+        overall_score = int(overall_score)
 
     tag_filter, tag_score = "", 0
     # if the user filters the cases by tags or time, then the view needs to be updated
@@ -109,18 +112,25 @@ def view_profile(request):
         total_tag_score = sum([a*b for a,b in zip(tag_average, tag_attempts)])
         total_tag_tries = sum(tag_attempts)
         tag_score = float("{0:.2f}".format(total_tag_score/total_tag_tries))
+        # better formats the tag_score
+        if tag_score % 1 == 0:
+            tag_score = int(tag_score)
 
 
     # all of these are calculated per case to be shown
-    total_average, user_average, user_attempts, total_attempts, tags = [], [], [], [], []
+    # total_average, user_average, user_attempts, total_attempts, tags = [], [], [], [], []
     for case in cases:
-        total_average.append(case.get_average_score())
-        user_average.append(case.get_average_score(user=request.user))
-        user_attempts.append(len(Attempt.objects.filter(case_study=case, user=request.user)))
-        total_attempts.append(len(Attempt.objects.filter(case_study=case)))
-        tags.append(TagRelationship.objects.filter(case_study=case))
+        case.total_average = case.get_average_score()
+        if case.total_average % 1 == 0: # if it is a whole number
+            case.total_average = int(case.total_average)
+        case.user_average = case.get_average_score(user=request.user)
+        if case.user_average % 1 == 0: # if it is a whole number
+            case.user_average = int(case.user_average)
+        case.user_attempts = len(Attempt.objects.filter(case_study=case, user=request.user))
+        case.total_attempts = len(Attempt.objects.filter(case_study=case))
+        case.tags = TagRelationship.objects.filter(case_study=case)
 
-    cases = list(zip(cases, total_average, user_average, user_attempts, total_attempts, tags))
+    # cases = list(zip(cases, total_average, user_average, user_attempts, total_attempts, tags))
 
     c = {
         'cases' : cases,
