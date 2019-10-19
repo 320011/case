4bb4dbf2ef9135cc184288c407d859de6686b010
Dragonite <haolin97@gmail.com>
2019-10-19 15:12:34 +0800

    Check for case_list before creating a playlist, add refresh and delete views

--- core/case_study/views/case_playlists.py
+++ core/case_study/views/case_playlists.py
@@ -20,6 +20,7 @@ def playlist_landing(request):
     return render(request, "playlist_landing.html", c)
 
 
+@login_required
 def create_new_playlist(request):
     tag_id = request.POST.get('tag_id', None)
     if tag_id:
@@ -27,16 +28,53 @@ def create_new_playlist(request):
         case_list = list(
             CaseStudy.objects.filter(case_state=CaseStudy.STATE_PUBLIC, tagrelationship__tag=tag).values_list('id',
                                                                                                               flat=True))
-        shuffle(case_list)
-        case_string = ','.join(str(e) for e in case_list)
-        Playlist.objects.create(owner=request.user, tag=tag, date_created=timezone.now(), case_list=case_string)
+        if case_list:
+            shuffle(case_list)
+            case_string = ','.join(str(e) for e in case_list)
+            new_playlist = Playlist.objects.create(owner=request.user, tag=tag, date_created=timezone.now(), case_list=case_string)
     else:
         tag = None
         case_list = list(CaseStudy.objects.filter(case_state=CaseStudy.STATE_PUBLIC).values_list('id', flat=True))
-        shuffle(case_list)
-        case_string = ','.join(str(e) for e in case_list)
-        Playlist.objects.create(owner=request.user, tag=tag, date_created=timezone.now(), case_list=case_string)
+        if case_list:
+            shuffle(case_list)
+            case_string = ','.join(str(e) for e in case_list)
+            new_playlist = Playlist.objects.create(owner=request.user, tag=tag, date_created=timezone.now(), case_list=case_string)
+    if case_list:
+        return JsonResponse({
+            "success": True,
+            "message": "Your playlist was successfully created!"
+        })
+    return JsonResponse({
+        "success": False,
+        "message": "An error has occurred. This is likely due to there being no cases for the specified tag. Please try again."
+    })
+
+
+@login_required
+def refresh_playlist(request):
+    playlist_id = request.POST.get('playlist_id', None)
+    playlist = get_object_or_404(Playlist, pk=int(playlist_id))
+    if not playlist.case_list:
+        return JsonResponse({
+            "success": False,
+            "message": "Your playlist does not contain any cases. Please recreate your playlist."
+        })
+    current_case_list = playlist.case_list.split(',')
+    shuffle(current_case_list)
+    new_case_list = ','.join(str(e) for e in current_case_list)
+    playlist.case_list = new_case_list
+    playlist.current_position = 0
+    playlist.save()
     return JsonResponse({
         "success": True,
-        "message": "Your playlist was successfully created!"
+        "message": "Your playlist was successfully refreshed!"
     })
+
+
+@login_required
+def delete_playlist(request):
+    playlist_id = request.POST.get('playlist_id', None)
+    playlist = get_object_or_404(Playlist, pk=int(playlist_id))
+    playlist.delete()
+    success = True if not playlist.id else False
+    return JsonResponse({'success': success})
