2c6c151cfaef0164f70b4513c847e80cc732fed9
Dragonite <haolin97@gmail.com>
2019-10-19 11:54:24 +0800

    Add form to playlist landing views.py

--- core/case_study/views/case_playlists.py
+++ core/case_study/views/case_playlists.py
@@ -5,13 +5,16 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from ..models import Tag, TagRelationship, CaseStudy, MedicalHistory, Medication, Attempt, Comment, Other, Question, Playlist
+from ..forms import PlaylistTagForm
 
 
 
 @login_required
 def playlist_landing(request):
     playlists = Playlist.objects.filter(owner=request.user)
+    form = PlaylistTagForm()
     c = {
-        "playlists": playlists
+        "playlists": playlists,
+        "form": form
     }
     return render(request, "playlist_landing.html", c)
