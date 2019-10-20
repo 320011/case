22d46a842860287c15f9a184f551242646a89a76
Tim <tim@tim-ings.com>
2019-10-20 22:01:57 +0800

    Prevents non-staff users accessing analytics

--- core/analytics/views.py
+++ core/analytics/views.py
@@ -7,6 +7,7 @@ from case_study.models import Question, \
     Comment, CommentReport
 from accounts.models import User
 from .forms import TagForm
+from core.decorators import staff_required
 
 
 def export_queryset_csv(qs, filename):
@@ -34,10 +35,13 @@ def export_queryset_csv(qs, filename):
     return response
 
 
+@staff_required
 def view_landing(request):
     c = {"form": TagForm }
     return render(request, "analytics-landing.html", c)
 
+
+@staff_required
 def tag_performance(request):
     tag_id = request.GET.get('tag_id', None)
     tag = get_object_or_404(Tag, pk=tag_id)
@@ -54,46 +58,57 @@ def tag_performance(request):
 
 
 
+@staff_required
 def view_question(request):
     return export_queryset_csv(Question.objects.all(), "uwacase_questions.csv")
 
 
+@staff_required
 def view_tag(request):
     return export_queryset_csv(Tag.objects.all(), "uwacase_tags.csv")
 
 
+@staff_required
 def view_medicalhistory(request):
     return export_queryset_csv(MedicalHistory.objects.all(), "uwacase_medicalhistories.csv")
 
 
+@staff_required
 def view_comment(request):
     return export_queryset_csv(Comment.objects.all(), "uwacase_comments.csv")
 
 
+@staff_required
 def view_casestudy(request):
     return export_queryset_csv(CaseStudy.objects.all(), "uwacase_casestudies.csv")
 
 
+@staff_required
 def view_medication(request):
     return export_queryset_csv(Medication.objects.all(), "uwacase_medications.csv")
 
 
+@staff_required
 def view_tagrelationship(request):
     return export_queryset_csv(TagRelationship.objects.all(), "uwacase_tagrelationships.csv")
 
 
+@staff_required
 def view_other(request):
     return export_queryset_csv(Other.objects.all(), "uwacase_others.csv")
 
 
+@staff_required
 def view_commentreport(request):
     return export_queryset_csv(CommentReport.objects.all(), "uwacase_commentreports.csv")
 
 
+@staff_required
 def view_attempt(request):
     return export_queryset_csv(Attempt.objects.all(), "uwacase_attempts.csv")
 
 
+@staff_required
 def view_user(request):
     return export_queryset_csv(User.objects.all(), "uwacase_users.csv")
 
